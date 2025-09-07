1f81b83f91942140ffd8a3146598797cf494e210
rinckodev <guilhermericardopro71@gmail.com>
2025-09-07 14:37:48 -0400

    update openapi docs layout

--- scripts/openapi/gen.ts
+++ scripts/openapi/gen.ts
@@ -1,19 +1,22 @@
-import { generateFiles } from "fumadocs-openapi";
+import { generateFiles, type Config } from "fumadocs-openapi";
 import { openapi } from "@/lib/openapi";
 
-void generateFiles({
-  input: openapi.pt,
-  output: "./content/docs/api/(generated)",
+const defaults = {
   includeDescription: true,
-});
+  output: "./content/docs/api/(generated)",
+  per: "tag",
+} satisfies Partial<Config>;
 
 void generateFiles({
+  ...defaults,
+  input: openapi.pt,
+});
+void generateFiles({
+  ...defaults,
   input: openapi.en,
-  output: "./content/docs/api/(generated)",
   beforeWrite(files) {
     for (const file of files) {
       file.path = file.path.replace(".mdx", ".en.mdx")
     }
   },
-  includeDescription: true,
 });
